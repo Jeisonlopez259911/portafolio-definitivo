efbde5633693dcc435ea5d59aef6ec7e04ca558e
Jeison Stiven Lopez Castro <jeisonlopez2599@gmail.com>
2025-01-13 14:14:10 -0500

    Se hizo un cambio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import {
   initTWE,
 } from "tw-elements";
 import { NavLink } from "react-router-dom";
+import HojaDeVida from "../assets/documents/HojaDeVida.pdf";
 
 
 
@@ -56,11 +57,12 @@ function Header() {
                 <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
                   <NavLink
                     className="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                    to="/contacto"
+                    to={HojaDeVida}
                     data-twe-nav-link-ref
                     data-twe-ripple-init
                     data-twe-ripple-color="light"
-                  >Features</NavLink>
+                    target="_blank"
+                  >Hoja de vida</NavLink>
                 </li>
                 <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
                   <NavLink
