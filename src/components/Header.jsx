1fc8af1a82e60563c8e878f598b872df5fe7eea1
Jeison Stiven Lopez Castro <jeisonlopez2599@gmail.com>
2025-01-16 13:01:36 -0500

    Un cambio pequeño

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -52,7 +52,7 @@ function Header() {
                 data-twe-navbar-nav-ref>
                 <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
                   <NavLink
-                    className="whitespace-pre block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+                    className="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                     to="/"
                     data-twe-nav-link-ref
                     data-twe-ripple-init
